877a64b489f212d53164f7fa11a09d9055e8a7de
Column01 <Column01Dev@gmail.com>
2025-06-27 20:52:38 -0400

    Structure File Optimizations

--- schem.py
+++ schem.py
@@ -1,4 +1,6 @@
 import nbtlib
+
+from time import perf_counter_ns
 from nbtlib import tag
 
 
@@ -65,28 +67,34 @@ def write_structure(blocks_dict, filename, data_version=3953):
         for (x, y, z), block in blocks_dict.items()
     }
 
-    palette = {"minecraft:air":0}
+    palette = {"minecraft:air": nbtlib.Int(0)}
     for block in sorted(set(norm_blocks.values())):
-        palette[block] = len(palette)
+        palette[block] = nbtlib.Int(len(palette))
+
+    # Precompute a list of NBT lib ints for speedup
+    n_ints = [nbtlib.Int(x) for x in range(max([width, height, length]))]
 
     blocks = []
-
-    # Fill the blocks list with air first
+    t0 = perf_counter_ns()
+    # Fill the blocks list with air first, and if there is a block in that spot put it there.
     for x in range(width):
         for y in range(height):
             for z in range(length):
                 block = norm_blocks.get((x, y, z))
-                palette_id = 0
                 if block:
                     palette_id = palette[block]
+                else:
+                    palette_id = palette.get("minecraft:air")
                 blocks.append(
                     nbtlib.Compound({
-                        "pos": nbtlib.List([nbtlib.Int(x), nbtlib.Int(y), nbtlib.Int(z)]),
-                        "state": nbtlib.Int(palette_id)
+                        "pos": nbtlib.List([n_ints[x], n_ints[y], n_ints[z]]),
+                        "state": palette_id
                     })
                 )
+    t1 = perf_counter_ns()
+    print(f"Schematic Block Fill: {int((t1-t0)/1000000)}ms")
 
-
+    t0 = perf_counter_ns()
     blocks = nbtlib.List(blocks)
     nbt_data = nbtlib.Compound(
         {
@@ -102,6 +110,11 @@ def write_structure(blocks_dict, filename, data_version=3953):
             "DataVersion": nbtlib.Int(data_version),
         }
     )
+    t1 = perf_counter_ns()
+    print(f"Building NBT Data: {int((t1-t0)/1000000)}ms")
 
-    nbt_file = nbtlib.File(nbt_data, filename=filename, gzipped=True)
+    t0 = perf_counter_ns()
+    nbt_file = nbtlib.File(nbt_data, filename=filename)
     nbt_file.save()
+    t1 = perf_counter_ns()
+    print(f"Compressing and Saving NBT Data: {int((t1-t0)/1000000)}ms")
